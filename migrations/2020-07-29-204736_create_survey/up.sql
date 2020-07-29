8a895d30c21481021db5de867045c5e29df54f32
Alexander Launi <alex.launi@gmail.com>
2020-07-29 18:05:19 -0400

    Create TABLE survey_data with JSON type instead of Varchar

--- migrations/2020-07-29-204736_create_survey/up.sql
+++ migrations/2020-07-29-204736_create_survey/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE survey_results (
     id SERIAL PRIMARY KEY,
     device_id UUID NOT NULL REFERENCES users(device_id),
     submitted TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    survey_data VARCHAR NOT NULL
+    survey_data JSON NOT NULL
 )
