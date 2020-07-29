22684b0439c8b6f0bc29a84e711378a94bb448f0
Alexander Launi <alex.launi@gmail.com>
2020-07-29 17:02:32 -0400

    Rename filters::repetitions to filters::rest_api
    filters::repetitions does not appropriately describe function.
    Reformat main.rs with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::env;
 use clap::{crate_version, value_t, App, Arg};
+use std::env;
 
 fn main() {
     const DEFAULT_PORT: u32 = 3030;
@@ -18,7 +18,7 @@ fn main() {
                 .value_name("PORT"),
         )
         .get_matches();
-        
+
     let port = value_t!(opts.value_of("port"), u32).unwrap_or(DEFAULT_PORT);
 
     if env::var_os("RUST_LOG").is_none() {
@@ -32,36 +32,39 @@ fn main() {
 }
 
 mod webserver {
-    use warp::Filter;
     use super::filters;
     use std::net::SocketAddrV4;
-    
+    use warp::Filter;
+
     #[tokio::main]
     pub async fn run(port: u32) {
         let db = liftright_data_server::establish_connection();
 
-        let api = filters::repetitions(db);
+        let api = filters::rest_api(db);
         let routes = api.with(warp::log("liftright_data_server"));
 
-        let addr: SocketAddrV4 = format!("0.0.0.0:{}", port).parse()
+        let addr: SocketAddrV4 = format!("0.0.0.0:{}", port)
+            .parse()
             .expect("Could not create IP.");
-        
+
         warp::serve(routes).run(addr).await
     }
 }
 
 mod filters {
+    use super::handlers;
     use uuid::Uuid;
     use warp::Filter;
-    use super::handlers;
 
-    use liftright_data_server::repetition::{NewRepetition};
+    use liftright_data_server::repetition::NewRepetition;
     use liftright_data_server::{DbPool, DbPooledConnection};
 
-    pub fn repetitions(db: DbPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    pub fn rest_api(
+        db: DbPool,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         repetitions_create(db.clone())
-        .or(rtfb_status(db))
-        .or(hello())
+            .or(rtfb_status(db))
+            .or(hello())
     }
 
     fn hello() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -70,7 +73,9 @@ mod filters {
             .and_then(handlers::heartbeat)
     }
 
-    fn repetitions_create(db: DbPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    fn repetitions_create(
+        db: DbPool,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::path!("v1" / "add_repetition")
             .and(warp::post())
             .and(json_body())
@@ -78,7 +83,9 @@ mod filters {
             .and_then(handlers::create_repetition)
     }
 
-    fn rtfb_status(db: DbPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    fn rtfb_status(
+        db: DbPool,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::path!("v1" / "rtfb_status" / Uuid)
             .and(warp::get())
             .and(with_db(db))
@@ -91,48 +98,59 @@ mod filters {
         warp::body::content_length_limit(1024 * 16).and(warp::body::json())
     }
 
-    fn with_db(pool: DbPool) -> impl Filter<Extract = (DbPooledConnection,), Error = warp::reject::Rejection> + Clone {
+    fn with_db(
+        pool: DbPool,
+    ) -> impl Filter<Extract = (DbPooledConnection,), Error = warp::reject::Rejection> + Clone {
         warp::any()
             .map(move || pool.clone())
             .and_then(|pool: DbPool| async move {
                 match pool.get() {
                     Ok(conn) => Ok(conn),
-                    Err(_) => Err(warp::reject())
+                    Err(_) => Err(warp::reject()),
                 }
             })
     }
 }
 
 mod handlers {
-    use warp::http;
     use uuid::Uuid;
+    use warp::http;
 
+    use liftright_data_server::repetition::{NewRepetition, Repetition};
     use liftright_data_server::user::User;
     use liftright_data_server::DbPooledConnection;
-    use liftright_data_server::repetition::{Repetition, NewRepetition};
 
     pub async fn heartbeat() -> Result<impl warp::Reply, warp::Rejection> {
-        let now = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH).expect("System time is before epoch!").as_secs();
+        let now = std::time::SystemTime::now()
+            .duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .expect("System time is before epoch!")
+            .as_secs();
         Ok(warp::reply::with_status(
             format!("{}", now),
-            http::StatusCode::OK
+            http::StatusCode::OK,
         ))
     }
 
-    pub async fn create_repetition(rep: NewRepetition, conn: DbPooledConnection) -> Result<impl warp::Reply, warp::Rejection> {
+    pub async fn create_repetition(
+        rep: NewRepetition,
+        conn: DbPooledConnection,
+    ) -> Result<impl warp::Reply, warp::Rejection> {
         match Repetition::create(&conn, rep) {
             Ok(_) => Ok(warp::reply::with_status(
                 "good job",
                 http::StatusCode::CREATED,
             )),
-            Err(_) => Err(warp::reject())
+            Err(_) => Err(warp::reject()),
         }
     }
 
-    pub async fn rtfb_status(uuid: Uuid, conn: DbPooledConnection) -> Result<impl warp::Reply, std::convert::Infallible> {
+    pub async fn rtfb_status(
+        uuid: Uuid,
+        conn: DbPooledConnection,
+    ) -> Result<impl warp::Reply, std::convert::Infallible> {
         match User::check_rtfb_status(&conn, &uuid) {
             Ok(rtfb) => Ok(warp::reply::json(&rtfb)),
-            Err(_) => Ok(warp::reply::json(&false))
+            Err(_) => Ok(warp::reply::json(&false)),
         }
     }
 }
