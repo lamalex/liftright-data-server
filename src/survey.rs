c8c2098e0ce903148f4058589ac6b279ea1e2d6a
Alexander Launi <alex.launi@gmail.com>
2020-08-23 12:00:21 -0400

    Add some json format verification tests

--- src/survey.rs
+++ src/survey.rs
@@ -45,13 +45,44 @@ pub fn submit(conn: &PgConnection, data: IncomingSurvey) -> Result<usize, Liftri
 mod tests {
     use super::*;
 
-    fn make_incoming_survey() -> IncomingSurvey {
-        let mut survey_data = HashMap::<String, Option<String>>::new();
-        survey_data.insert(
-            String::from("test question"),
-            Some(String::from("omg an answer!")),
+    #[test]
+    fn create_survey_from_incoming() {
+        let incoming = make_incoming_survey_from_valid();
+        let survey = SurveyData::from(incoming);
+        assert!(&survey.survey_data[..("ERROR PARSING MAP").len()] != "ERROR PARSING MAP")
+    }
+
+    #[test]
+    fn error_on_missing_device_id() {
+        let raw_json = format!(
+            "
+            {{
+                \"device_id\": \"\",
+                \"survey_data\": {}
+            }}
+            ",
+            make_survey_data()
         );
 
+        assert!(serde_json::from_str::<IncomingSurvey>(&raw_json).is_err())
+    }
+
+    #[test]
+    fn error_on_missing_survey() {
+        let raw_json = format!(
+            "
+            {{
+                \"device_id\": \"{}\",
+                \"survey_data\": \"\"
+            }}
+            ",
+            Uuid::new_v4().to_string()
+        );
+
+        assert!(serde_json::from_str::<IncomingSurvey>(&raw_json).is_err())
+    }
+
+    fn make_incoming_survey_from_valid() -> IncomingSurvey {
         let raw_json = format!(
             "
             {{
@@ -60,15 +91,27 @@ mod tests {
             }}
             ",
             Uuid::new_v4().to_string(),
-            serde_json::to_string(&survey_data).unwrap()
+            make_survey_data()
         );
 
         serde_json::from_str(&raw_json).unwrap()
     }
-    #[test]
-    fn create_survey_from_incoming() {
-        let incoming = make_incoming_survey();
-        let survey = SurveyData::from(incoming);
-        assert!(&survey.survey_data[..("ERROR PARSING MAP").len()] != "ERROR PARSING MAP")
+
+    fn make_survey_data() -> String {
+        let mut survey_data = HashMap::<String, Option<String>>::new();
+        survey_data.insert(
+            String::from("Was the game fun?"),
+            Some(String::from("Very")),
+        );
+        survey_data.insert(
+            String::from("Was the sensor/arm band comfortable?"),
+            Some(String::from("Somewhat")),
+        );
+        survey_data.insert(
+            String::from("What Metrics do you find useful to guage your performance over time?"),
+            Some(String::from("I 💪🏻 my 🍆 and 🍑 into a 🐉")),
+        );
+
+        serde_json::to_string(&survey_data).unwrap()
     }
 }
