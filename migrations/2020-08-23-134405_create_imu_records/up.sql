89ddf98379a6d912804bf752ce8a6cd3810cbb9c
Alexander Launi <alex.launi@gmail.com>
2020-08-24 20:38:04 -0400

    Imu record holds a reference to its pair_id instead of pair holding an id of a record

--- migrations/2020-08-23-134405_create_imu_records/up.sql
+++ migrations/2020-08-23-134405_create_imu_records/up.sql
@@ -1,14 +1,15 @@
+CREATE TABLE imu_record_pairs (
+    id SERIAL PRIMARY KEY,
+    session_id UUID NOT NULL REFERENCES sessions(id)
+);
+
 CREATE TABLE imu_records (
     id SERIAL PRIMARY KEY,
     x real NOT NULL,
     y real NOT NULL,
     z real NOT NULL,
-    time TIMESTAMP WITH TIME ZONE NOT NULL
+    time TIMESTAMP WITH TIME ZONE NOT NULL,
+    pair_id SERIAL REFERENCES imu_record_pairs(id)
 );
 
-CREATE TABLE imu_record_pairs (
-    id SERIAL PRIMARY KEY,
-    session_id UUID NOT NULL REFERENCES sessions(id),
-    acc SERIAL REFERENCES imu_records(id),
-    gyro SERIAL REFERENCES imu_records(id)
-);
+
