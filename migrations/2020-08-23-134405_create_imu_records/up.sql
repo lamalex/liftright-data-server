852802019543664b238788520b0285dac9aec7eb
Alexander Launi <alex.launi@gmail.com>
2020-08-24 22:22:23 -0400

    Go back to imu_record_pairs holding acc and gyro ids. Actually do db insertion. Seems to be working

--- migrations/2020-08-23-134405_create_imu_records/up.sql
+++ migrations/2020-08-23-134405_create_imu_records/up.sql
@@ -1,15 +1,14 @@
-CREATE TABLE imu_record_pairs (
-    id SERIAL PRIMARY KEY,
-    session_id UUID NOT NULL REFERENCES sessions(id)
-);
-
 CREATE TABLE imu_records (
     id SERIAL PRIMARY KEY,
     x real NOT NULL,
     y real NOT NULL,
     z real NOT NULL,
-    time TIMESTAMP WITH TIME ZONE NOT NULL,
-    pair_id SERIAL REFERENCES imu_record_pairs(id)
+    time TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-
+CREATE TABLE imu_record_pairs (
+    id SERIAL PRIMARY KEY,
+    session_id UUID NOT NULL REFERENCES sessions(id),
+    acc SERIAL REFERENCES imu_records(id),
+    gyro SERIAL REFERENCES imu_records(id)
+);
